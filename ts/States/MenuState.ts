efbd8e9dee161ce72b3c765026accad56bf5454b
RauscherTasha <natascharauscher@gmx.net>
2015-04-18 21:42:54 +0200

    sound preload

--- ts/States/MenuState.ts
+++ ts/States/MenuState.ts
@@ -1,19 +1,19 @@
 /// <reference path="../../phaserLib/phaser.d.ts"/>
 /// <reference path="../GameObjects/Bug.ts"/>
 
-class MenuState extends Phaser.State
-{
-    game: Phaser.Game;
+class MenuState extends Phaser.State {
+    game:Phaser.Game;
 
-    START_BUTTON: Phaser.Key;
+    START_BUTTON:Phaser.Key;
 
 
-    bugs: Array<Bug>;
+    bugs:Array<Bug>;
+    choosenBugs:Array<String>;
+    choosen:Array <boolean>;
 
-    create()
-    {
+    create() {
         var line1 = "MENU";
-        var style = { font: "48px Arial", fill: "#ff0000", textAlign: "center"};
+        var style = {font: "48px Arial", fill: "#ff0000", textAlign: "center"};
         this.game.add.text(10, 10, line1, style);
 
         this.START_BUTTON = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
@@ -21,16 +21,51 @@ class MenuState extends Phaser.State
 
         this.bugs = [
 
-            new Bug(this.game,"BUG1_MOVING", 0, this.game.height - this.game.height/2),
-            new Bug(this.game,"BUG2_MOVING", 400, this.game.height - this.game.height/2),
-            new Bug(this.game,"BUG3_MOVING", 800, this.game.height - this.game.height/2)
+            new Bug(this.game, "BUG1_MOVING", 150, this.game.height - this.game.height / 4 * 3),
+            new Bug(this.game, "BUG2_MOVING", 550, this.game.height - this.game.height / 4 * 3),
+            new Bug(this.game, "BUG3_MOVING", 950, this.game.height - this.game.height / 4 * 3),
+            new Bug(this.game, "BUG4_MOVING", 350, this.game.height - this.game.height / 2)
+            //new Bug(this.game,"BUG5_MOVING", 750, this.game.height - this.game.height/2)
+
 
         ];
-        
+        for (var i=0;i<this.bugs.length;i++) {
+
+            // add bug
+            this.bugs[i].scale.x = 0.3;
+            this.bugs[i].scale.y = 0.3;
+            this.game.add.existing(this.bugs[i]); // add bird to scene
+        }
+        for(var i = 0; i < this.bugs.length; i++) {
+            this.bugs[i].inputEnabled = true;
+            this.bugs[i].input.start(1, true);
+        }
+
+
     }
 
-    buttonPressed()
+    /*update()
     {
+        for(var i = 0; i<this.bugs.length;i++)
+        {
+            if(this.choosen[i] == false)
+            {
+                console.log("enter!");
+                this.bugs[i].Animate();
+                this.choosen[i] = true;
+            }
+            else if( this.choosen[i] == true)
+            {
+                console.log("enter2!");
+                this.bugs[i].animations.stop();
+                this.choosen[i] = false;
+            }
+
+        }
+    }
+
+    */
+    buttonPressed() {
         this.game.state.start("GameScreenState");
     }
 
--- ts/States/TitleScreenState.ts
+++ ts/States/TitleScreenState.ts
@@ -1,14 +1,12 @@
 /// <reference path="../../phaserLib/phaser.d.ts"/>
-class TitleScreenState extends Phaser.State
-{
-    game: Phaser.Game;
+class TitleScreenState extends Phaser.State {
+    game:Phaser.Game;
 
-    START_BUTTON: Phaser.Key;
+    START_BUTTON:Phaser.Key;
 
-    create()
-    {
+    create() {
         var line1 = "Bugs and Features";
-        var style = { font: "48px Arial", fill: "#ff0000", textAlign: "center"};
+        var style = {font: "48px Arial", fill: "#ff0000", textAlign: "center"};
         this.game.add.text(10, 10, line1, style);
 
         this.START_BUTTON = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
@@ -16,8 +14,7 @@ class TitleScreenState extends Phaser.State
 
     }
 
-    buttonPressed()
-    {
+    buttonPressed() {
         this.game.state.start("MenuState");
     }
 
