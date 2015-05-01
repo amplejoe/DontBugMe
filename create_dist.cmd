62b61a288df0a4615b60680771eca7710a011ce3
unknown <amplejoe@gmail.com>
2015-05-01 06:20:07 +0200

    finished create_dist.bat: execute to create distribution folder

--- create_dist.cmd
+++ create_dist.cmd
@@ -1,16 +1,61 @@
 :: creates a distribution folder with all needed files to distribute the game
-
-if not exist dist mkdir dist
-
+@echo off
+:: clean directory
+echo ################## Cleaning up... #######################
+if exist dist rd /s /q dist 
+mkdir dist
+mkdir dist\phaserLib
+echo ################## Copying files... #####################
 :: copy assts/css/needed files
 xcopy assets dist\assets\ /s /e /Y
 xcopy css dist\css\ /s /e /Y
 copy info.html dist
+copy phaserLib\phaser.min.js dist\phaserLib
+copy phaserLib\phaser.map dist\phaserLib
 
-:: TODO modify index.html (replace js files by game.min.js) 
-:: FOR /F "eol=; tokens=2,3* delims=, " %i in (index.html) do @echo %i %j %k
+echo ################## Modifying index.html ##################
+:: modify index.html (replace js files by game.min.js) 
+:: important enable delayed expansion so writeline can be set in loop (use !var! to check)
+setlocal enabledelayedexpansion
+set writeline=true
+for /f "tokens=*" %%a in (index.html) do (	
 
-:: Packs and minifies all js files into dist\js\game.min.js
-if not exist dist\js mkdir dist\js
-java -jar "tools\closure\compiler.jar" --js js\Utils\GameSettings.js --js js\Utils\UtilFunctions.js  --js js\Utils\Timer.js  --js js\Utils\IntervalTimer.js  --js js\Utils\RandomIntervalTimer.js  --js js\Utils\CountdownTimer.js --js js\GameObjects\Bug.js --js js\States\TitleScreenState.js --js js\States\MenuState.js --js js\States\GameScreenState.js --js js\States\GameOverScreenState.js --js js\app.js --js_output_file dist\js\game.min.js 
+		:: check if html annotation for following js files is found
+		if %%a == ^<^!--^ START_APP_JS^ --^> (
+			:: append to new dist\index.html
+			echo ^<script^ src="phaserLib/phaser.min.js"^>^</script^> >> dist\index.html
+			echo ^<script^ src="js/game.min.js"^>^</script^>  >> dist\index.html			
+			set writeline=false
+		)
+		
+		:: append to new dist\index.html (check for doctype and add '!' -> removed somehow due to delayed expansions)
+		if !writeline! == true (
+			if %%a == ^<DOCTYPE^ html^> (
+				echo ^<^^!DOCTYPE^ html^> >> dist\index.html
+			)	else	 (
+				echo %%a >> dist\index.html
+			)
+		)
+		if %%a == ^<^!--^ END_APP_JS^ --^> (
+			set writeline=true
+		)
+)
 
+echo ################## Minifying js files #####################
+:: Packs and minifies all js files into dist\js\game.min.js 
+mkdir dist\js
+java -jar "tools\closure\compiler.jar" ^
+       --js js\Utils\GameSettings.js ^
+	   --js js\Utils\UtilFunctions.js  ^
+	   --js js\Utils\Timer.js  ^
+	   --js js\Utils\IntervalTimer.js  ^
+	   --js js\Utils\RandomIntervalTimer.js  ^
+	   --js js\Utils\CountdownTimer.js ^
+	   --js js\GameObjects\Bug.js ^
+	   --js js\States\TitleScreenState.js ^
+	   --js js\States\MenuState.js ^
+	   --js js\States\GameScreenState.js ^
+	   --js js\States\GameOverScreenState.js ^
+	   --js js\app.js ^
+	   --js_output_file dist\js\game.min.js 
+echo ################## Finished! ##############################
