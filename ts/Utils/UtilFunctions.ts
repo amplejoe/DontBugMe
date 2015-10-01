883d56641cd4f1c0c4fce05091223c46d5188119
amplejoe <amplejoe@gmail.com>
2015-10-02 00:59:10 +0200

    added local storage highscore capabilities

--- ts/Utils/UtilFunctions.ts
+++ ts/Utils/UtilFunctions.ts
@@ -31,6 +31,76 @@ module Utils
             return decodeURIComponent(encodeURIComponent(specialCharacter)).toString();
         }
 
+        static getHighScoresTable(): string
+        {
+            var highScoreSize =  parseInt(localStorage["dontbugme.highscore.count"]);
+            var highscoresString = "<table id='highscoreTable' class='centered'><thead><tr><th>Player</th><th>Score</th><th>Time</th></tr></thead>";
+
+            for (var i=0;i<highScoreSize;i++)
+            {
+                var oddClass = (i % 2 == 0)? "" : " class='odd'";
+                highscoresString += "<tbody><tr"+oddClass+">";
+                highscoresString += "<th>"+(i+1)+"</th>";
+                highscoresString += "<td>"+localStorage["dontbugme.highscore."+i+".score"]+"</td>";
+                highscoresString += "<td>"+localStorage["dontbugme.highscore."+i+".time"]+"</td>";
+                highscoresString += "</tr></tbody>";
+            }
+
+            highscoresString += "<tfoot><th>Total</th><td colspan='2'>"+highScoreSize+" Scores</td></tfoot></table>";
+
+            return highscoresString;
+            //document.getElementById('highscores').innerHTML = highscoresString;
+        }
+
+
+        static addHighscoreEntrySorted(score: number, time:string ): number
+        {
+            var highscoreEntryCountString = localStorage["dontbugme.highscore.count"];
+            var id = 0;
+
+            if (highscoreEntryCountString == null)
+            {
+                localStorage["dontbugme.highscore.count"] = 0;
+            }
+            else
+            {
+                var id = parseInt(highscoreEntryCountString);
+            }
+
+            var currentHighscoreCount = parseInt(localStorage["dontbugme.highscore.count"]);
+
+            if (currentHighscoreCount > 0)
+            {
+                var insert: boolean = false;
+                for (var i=0;i<currentHighscoreCount;i++)
+                {
+                    var curScore: number = parseInt(localStorage["dontbugme.highscore."+i+".score"]);
+                    if (score >= curScore)
+                    {
+                        id = i;
+                        insert = true;
+                        break;
+                    }
+                }
+            }
+
+            if (insert)
+            {
+                // shift scores to make room for new one
+                for (var i = (currentHighscoreCount-1); i >= id; i--)
+                {
+                    localStorage["dontbugme.highscore."+(i+1)+".score"] = localStorage["dontbugme.highscore."+i+".score"];
+                    localStorage["dontbugme.highscore."+(i+1)+".time"] = localStorage["dontbugme.highscore."+i+".time"];
+                }
+            }
+
+            // add current score
+            localStorage["dontbugme.highscore."+id+".score"] = score;
+            localStorage["dontbugme.highscore."+id+".time"] = time;
+            localStorage["dontbugme.highscore.count"] = currentHighscoreCount + 1;
+            return (id+1);
+        }
+
         static toInt(value) { return ~~value; }
 
     }
